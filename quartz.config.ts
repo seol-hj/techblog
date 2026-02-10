5560ccab07b9cb171be7009efa11abb587ebe30d
seol-hj <ganddoree@gmail.com>
2026-02-10 11:08:03 +0900

    blog title 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "☁️Blog☁️",
+    pageTitle: "ls -a /blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
