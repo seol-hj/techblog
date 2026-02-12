8df1fab270a5d049c32886989c50ff2278088444
seol-hj <ganddoree@gmail.com>
2026-02-12 10:13:03 +0900

    index.md 링크

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -59,7 +59,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({folderClickBehavior: "link",}),
   ],
   right: [
     Component.Graph(),
@@ -83,7 +83,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({folderClickBehavior: "link",}),
   ],
   right: [],
 }
