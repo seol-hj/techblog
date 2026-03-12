07a33883ed07a38aea99c3ef979e57aaf74c61ec
seol-hj <ganddoree@gmail.com>
2026-03-12 09:24:03 +0900

    조금 수정

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "cd /home/hj/blog",
+    pageTitle: "cd",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
